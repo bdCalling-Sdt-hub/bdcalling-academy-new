f4be1376fa51bb6a05687b1f4686be418cca5e5c
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-12-09 15:33:43 +0600

    s

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const cloud = true;
 const imgCloud = true;
 
-const url = 'http://165.232.137.110/api';
+const url = 'http://165.232.137.110:8000/api';
 
-const imageUrl = 'http://165.232.137.110';
+const imageUrl = 'http://165.232.137.110:8000';
 // const url = 'https://mainserver.bdcallingacademy.com/api';
 
 // const imageUrl = 'https://mainserver.bdcallingacademy.com';
